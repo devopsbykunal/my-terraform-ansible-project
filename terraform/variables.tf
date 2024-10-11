945671e5204a615e975f9920a40076f6be665a55
KUNAL JOSHI <globalinformationtracker2002@gmail.com>
2024-10-11 21:30:04 +0800

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "eu-north-1"
+}
+
+variable "ami" {
+  default = "ami-097c5c21a18dc59ea" # Change as necessary
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
